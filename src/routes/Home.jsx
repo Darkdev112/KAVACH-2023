3472476054b6c64dde0f6db75ffe56f902add495
Jahnavi Lenka <jahnavi2003lenka@gmail.com>
2023-04-24 20:40:40 +0530

    header added

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from '../pages/LandingPage/LandingPage'
 import InsuranceFraud from '../pages/InsuranceFraud/InsuranceFraud'
+import Header from '../components/Loader/Header'
 
 const Home = () => {
   return (
     <Routes>
         <Route path='/' element={<LandingPage/>} />
         <Route path='/insurancefraud' element={<InsuranceFraud/>} />
+        <Route path='/header' element={<Header/>}/>
     </Routes>
   )
 }
