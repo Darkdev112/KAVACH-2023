08f9662b843eca7c41ce3f6e5cbbb1853c81e75d
Dev Ashrit Behera <devashritcode@gmail.com>
2023-04-25 23:00:23 +0530

    card implemented
    Signed-off-by: Dev Ashrit Behera <devashritcode@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Home from './routes/Home';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <>
       <Provider store={store}>
         <BrowserRouter>
-          <Home/>
+          <Layout>
+            <Home/>
+          </Layout>
         </BrowserRouter>
       </Provider>
     </>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from '../pages/LandingPage/LandingPage'
 import InsuranceFraud from '../pages/InsuranceFraud/InsuranceFraud'
-import Card from '../components/Card/Card'
 
 const Home = () => {
   return (
     <Routes>
         <Route path='/' element={<LandingPage/>} />
         <Route path='/insurancefraud' element={<InsuranceFraud/>} />
-        <Route path='/card' element={<Card/>}/>
     </Routes>
   )
 }
