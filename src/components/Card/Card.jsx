83ec746263f7f312034eb50f3cb35d4cad0ce1ac
Dev Ashrit Behera <devashritcode@gmail.com>
2023-04-26 18:32:44 +0530

    cards routing complete
    Signed-off-by: Dev Ashrit Behera <devashritcode@gmail.com>

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,30 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Card.scss'
-import Insurance from '../../../src/Assets/images/insurance.png'
-import Credit from '../../../src/Assets/images/credit.png'
-import Debit from '../../../src/Assets/images/debit.png'
-import Coin from '../../../src/Assets/images/coin.png'
-import Crypto from '../../../src/Assets/images/crypto.png'
-import Online from '../../../src/Assets/images/online.png'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router'
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../redux/slices/modalSlice';
 
-const Card = () => {
+const Card = ({img, link, userToken}) => {
+  const nav = useNavigate();
+  const dispatch = useDispatch()
+
+  const handleNav = () => {
+    if(userToken)
+    {
+      nav(`/${link}`)
+    }
+    else{
+      dispatch(openModal())
+    }
+  }
+  
+  
   return (
-    <div className='card-section'>
-      <div className="all-cards">
-      <h1>HEADING OF THE FRAUD TYPE</h1>
-      <div className="all-cards">
-        <div className="cards">
-          <Link to='/insurancefraud'><div className="card"><img src={Insurance} alt="" /></div></Link>
-          <Link to='/creditcardfraud'><div className="card"><img src={Credit} alt="" /></div></Link>
-          <Link to='/onlinefraud'><div className="card"><img src={Online} alt="" /></div></Link>
-          <Link to='/debitcardfraud'><div className="card"><img src={Debit} alt="" /></div></Link>
-          <Link to='/cryptofraud'><div className="card"><img src={Crypto} alt="" /></div></Link>
-          <Link to='/coinfraud'><div className="card"><img src={Coin} alt="" /></div></Link>
-        </div>
-      </div>
-      </div>
-    </div>
+        <div className="card"><img src={img} alt="" onClick={handleNav}/></div>
   )
 }
 
