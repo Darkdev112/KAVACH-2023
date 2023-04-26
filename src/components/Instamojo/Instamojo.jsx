a288b5ad27dec539e53361d4c233b8fc1c28a750
Paras Singh Bhatia <parasbhatia100@gmail.com>
2023-04-26 17:38:06 +0530

    Payment Gateway Done
    Signed-off-by: Paras Singh Bhatia <parasbhatia100@gmail.com>

---
+++ src/components/Instamojo/Instamojo.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Instamojo = () => {
+
+  const [name,setName] = useState("");
+  const [email, setEmail] = useState("")
+  const [amount,setAmount] = useState("")
+  const nav=useNavigate();
+
+  const handleSubmit=async()=>{
+    try {
+      const response = await axios.post('http://localhost:2000/pay',{name, email,amount},{});
+      console.log(response);
+      nav("/checkEmail");
+    } catch (error) {
+      console.log(error);
+    }
+    
+  }
+
+
+  return (
+    <div className="container">
+      <br />
+      <h1>Instamojo Payment</h1>
+      <form action="/pay">
+
+        <div className="form-group">
+          <input type="text" placeholder="Name" name="name" className="form-control" required onChange={(e) => setName(e.target.value)}/>
+        </div>
+
+        <div className="form-group">
+          <input type="email" placeholder="Email" name="email" className="form-control" required onChange={(e) => setEmail(e.target.value)}/>
+        </div>
+
+        <div className="form-group">
+          <input type="number" placeholder="Amount" name="amount" className="form-control" required onChange={(e) => setAmount(e.target.value)}/>
+        </div>
+
+        <div className="form-group">
+          <button class="btn" onClick={(e) => {e.preventDefault(); handleSubmit(); }}>
+               Generate Payment Link
+          </button>
+        </div>
+
+      </form>
+    </div>
+  )
+}
+
+export default Instamojo
