d9c7bb856a6fc668237fe2d4ffcc3d3c159f8a81
Abhijeet Sahu <abhijeetsahu2003@gmail.com>
2023-04-27 17:42:40 +0530

    AOS added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,17 @@ import './Header.scss'
 import character from '../../Assets/images/character.png'
 
 
-const Header = () => {
+const Header = ({aos,aos1}) => {
     return (
         <div>
             <div className='header'>
-                <div className='head-text'>
+                <div className='head-text' data-aos={`${aos1}`}>
                     <h1 className='heading'>LOREM EPSUM</h1>
                     <div className='parah-header'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, harum
                         quod, odio molestias ratione officia quis debitis ab, omnis quas magnam consectetur
                         voluptatem pariatur unde illum doloribus provident quia dolore!</div>
                 </div>
-                <div className='head-image'>
+                <div className='head-image' data-aos={`${aos}`}>
                     <img src={character} alt="" />
                 </div>
             </div>
